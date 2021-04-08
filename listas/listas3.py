8983b70597c888cd4630bab992e589bf76dc35bb
Bruno Germano <brunoalvesgermano@gmail.com>
2021-04-07 22:17:49 -0300

    Fix gitignore

---
+++ listas/listas3.py
@@ -0,0 +1,16 @@
+lista = ["Guitarra", "Bateria", "Violão", "Baixo"]
+
+#len mostra quantos itens tem a lista
+#print(len(lista))
+
+#count mostra quantas vezes esse elemento indicado aparece na lista
+#print(lista.count("Guitarra"))
+
+#poe todos os elementos em ordem alfabetica
+#print(lista)
+#lista.sort()
+#print(lista)
+
+#inverte a ordem da lista
+lista.reverse()
+print(lista)
