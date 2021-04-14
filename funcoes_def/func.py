3b6579896527578aac5a1b6180a9ef485f2d2139
Bruno Germano <brunoalvesgermano@gmail.com>
2021-04-13 21:34:25 -0300

    Add functions example

---
+++ funcoes_def/func.py
@@ -0,0 +1,8 @@
+def somar():
+    a = float(input("Digite um valor "))
+    b = float(input("digite outro "))
+
+    soma = a + b
+    print(soma)
+
+somar()
---
+++ funcoes_def/func2.py
@@ -0,0 +1,8 @@
+def somar(a, b):
+    soma = a + b
+    print(soma)
+
+
+valor1 = float(input("Digite um valor "))
+valor2 = float(input("digite outro "))
+somar(valor1, valor2)
