bbbe5f22bb99aa5628f6ca798c2eaa038bae78e4
Bruno Germano <brunoalvesgermano@gmail.com>
2021-04-13 21:48:11 -0300

    Add functions example

---
+++ funcoes_def/func com return.py
@@ -0,0 +1,7 @@
+def somar(a, b):
+    soma = a + b
+    return soma
+
+a = somar(50, 30)
+
+print(a)
--- funcoes_def/importa_funcao.py
+++ funcoes_def/importa_funcao.py
@@ -1,6 +1,7 @@
 # para importar todas as funcoes do arquivo calc basta usar: import calc
 # abaixo é um exemplo de como importar somente a função somar
 
+#from calc import * < assim importa tudo
 from calc import somar
 
 teste = somar(10, 3)
