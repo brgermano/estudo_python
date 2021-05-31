ef67d9699f9dfac9ae572981b691675a2271f862
Bruno Germano <brunoalvesgermano@gmail.com>
2021-05-30 21:25:03 -0300

    Add dictionary example

---
+++ listas/dicionario4.py
@@ -0,0 +1,9 @@
+#criando o dicionário dos contatos
+contatos = {
+    "Clark Kent":
+        {"Celular":"123456",
+         "Email":"super@krypton.com"},
+    "Bruce Wayne":
+        {"Celular":"654321",
+         "Email":"bat@caverna.com.br"}
+}
